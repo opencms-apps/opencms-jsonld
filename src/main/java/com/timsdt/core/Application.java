664fee574ec0c6cff8a58ee28de73e3f4b03df77
Sac <sac.le@primedata.ai>
2022-06-28 11:30:15 +0700

    update types

--- src/main/java/com/timsdt/core/Application.java
+++ src/main/java/com/timsdt/core/Application.java
@@ -1,14 +1,23 @@
 package com.timsdt.core;
 
-import org.opencms.configuration.CmsConfigurationManager;
-import org.opencms.main.OpenCms;
-import org.xml.sax.SAXException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.timsdt.core.jsonld.Brand;
+import com.timsdt.core.jsonld.Organization;
+import com.timsdt.core.jsonld.Product;
 
 import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException, SAXException {
-        CmsConfigurationManager configs = new CmsConfigurationManager(OpenCms.getSystemInfo().getConfigFolder());
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
+        Product product = new Product();
+        product.productName = "Example Name";
+        product.setBrand(new Brand());
+
+        String personJsonLd = writer.writeValueAsString(product);
+        System.out.println(personJsonLd);
     }
 }
---
+++ src/main/java/com/timsdt/core/jsonld/ProductGroup.java
@@ -0,0 +1,8 @@
+package com.timsdt.core.jsonld;
+
+public class ProductGroup extends Schema {
+    @Override
+    public Types getType() {
+        return Types.ProductGroup;
+    }
+}
